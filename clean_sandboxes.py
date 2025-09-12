72cb96e3546e37d9491ba139ea34ea5928635be7
Mika Senghaas <mail@mikasenghaas.de>
2025-09-12 10:04:49 +0200

    Install 1p CLI

--- clean_sandboxes.py
+++ clean_sandboxes.py
@@ -1,6 +1,6 @@
 # /// script
 # requires-python = ">=3.12"
-# dependencies = ["prime>=0.3.23"]
+# dependencies = ["prime>=0.3.26"]
 # ///
 
 from prime_cli.api.client import APIClient
@@ -9,10 +9,10 @@ from prime_cli.api.sandbox import SandboxClient
 
 def main() -> None:
     client = SandboxClient(APIClient())
-    for sandbox in client.list().sandboxes:
-        if sandbox.user_id == "cmakj7hyo002rz091pdjngniy" and sandbox.status == "RUNNING":
-            print(f"Deleting sandbox {sandbox.id}")
-            client.delete(sandbox.id)
+    sandbox_ids = [sandbox.id for sandbox in client.list(per_page=100, exclude_terminated=True).sandboxes if sandbox.user_id ==  "cmakj7hyo002rz091pdjngniy"]
+    print(f"Deleting {len(sandbox_ids)} sandboxes")
+    client.bulk_delete(sandbox_ids)
+    print(f"Success!")
 
 
 if __name__ == "__main__":
