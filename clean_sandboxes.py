5c08c5d727f49cf6cbcf5531461d671ff683e716
Mika Senghaas <mail@mikasenghaas.de>
2025-09-14 11:01:31 +0200

    Ruff

--- clean_sandboxes.py
+++ clean_sandboxes.py
@@ -9,7 +9,11 @@ from prime_cli.api.sandbox import SandboxClient
 
 def main() -> None:
     client = SandboxClient(APIClient())
-    sandbox_ids = [sandbox.id for sandbox in client.list(per_page=100, exclude_terminated=True).sandboxes if sandbox.user_id ==  "cmakj7hyo002rz091pdjngniy"]
+    sandbox_ids = [
+        sandbox.id
+        for sandbox in client.list(per_page=100, exclude_terminated=True).sandboxes
+        if sandbox.user_id == "cmakj7hyo002rz091pdjngniy"
+    ]
     print(f"Deleting {len(sandbox_ids)} sandboxes")
     client.bulk_delete(sandbox_ids)
     print(f"Success!")
@@ -17,4 +21,3 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
-
